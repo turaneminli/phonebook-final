77f365c4debaf45debfcc24a8242bb646d395ce7
turaneminli <eminli.turan@gmail.com>
2022-03-14 14:26:33 +0200

    DB secret

--- backend/util/database.js
+++ backend/util/database.js
@@ -1,9 +1,13 @@
 const Sequelize = require("sequelize");
 require("dotenv").config();
 
-const database = process.env.SQL_DATABASE_NAME;
-const username = process.env.SQL_USERNAME;
-const password = process.env.SQL_PASSWORD;
+// const database = process.env.SQL_DATABASE_NAME;
+// const username = process.env.SQL_USERNAME;
+// const password = process.env.SQL_PASSWORD;
+
+const database = "phonebookdb"
+const username = "phonebook_admin"
+const password = "sqlMy2022Phonebook"
 
 const sequelize = new Sequelize(database, username, password, {
   host: "mydb.ch9lxjtadwif.us-east-1.rds.amazonaws.com",
